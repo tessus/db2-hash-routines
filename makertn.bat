1404c04d9b7ae39d7a70456ece69d7b260ed8a9e
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2014-05-07 20:41:09 -0400

    adjusted the windows build files

--- makertn.bat
+++ makertn.bat
@@ -8,8 +8,8 @@ set DB2PATH=c:\Program Files\IBM\sqllib
 set APRINC=c:\apr\include
 set APRLIB=c:\apr\lib
 
-cl -Od /nologo /c /I %APRINC% /D WIN32 hash.c
-link /libpath:%APRLIB% /subsystem:console /incremental:no /out:hash.dll /dll /def:hash.def hash.obj libapr-1.lib libaprutil-1.lib db2api.lib kernel32.lib user32.lib
+cl -Od /nologo /c /I %APRINC% /D WIN32 db2hash.c hash.c
+link /libpath:%APRLIB% /subsystem:console /incremental:no /out:hash.dll /dll /def:hash.def db2hash.obj hash.obj libapr-1.lib libaprutil-1.lib db2api.lib kernel32.lib user32.lib
 
 copy hash.dll "%DB2PATH%\function"
 
--- maketest.bat
+++ maketest.bat
@@ -8,7 +8,7 @@ set APRLIB=c:\apr\lib
 
 set LIB=%APRLIB%;%LIB%
 
-cl /nologo /c /I %APRINC% /D WIN32 test_hash.c
-link /libpath:%APRLIB% /subsystem:console /incremental:no /out:test_hash.exe test_hash.obj libapr-1.lib libaprutil-1.lib kernel32.lib user32.lib
+cl /nologo /c /I %APRINC% /D WIN32 test_hash.c hash.c
+link /libpath:%APRLIB% /subsystem:console /incremental:no /out:test_hash.exe test_hash.obj hash.obj libapr-1.lib libaprutil-1.lib kernel32.lib user32.lib
 
 @echo on
